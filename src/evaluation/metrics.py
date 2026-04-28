d9020ced9586fc113c1ab95965debb2b98559b1f
JaydannG <grossmanjaydann@gmail.com>
2026-04-28 08:56:55 -0400

    added function to calculate precision@k metric to evaluate the popularity recommender with

---
+++ src/evaluation/metrics.py
@@ -0,0 +1,12 @@
+def precision_at_k(recommended_items, relevant_items, k: int = 10):
+    recommended_k = recommended_items[:k]
+
+    if (len(recommended_k) == 0):
+        return 0.0
+
+    recommended_set = set(recommended_k)
+    relevant_set = set(relevant_items)
+
+    hits = recommended_set.intersection(relevant_set)
+
+    return len(hits) / k
