f0af65de8da45284c59871b3175a5add3e66649f
JaydannG <grossmanjaydann@gmail.com>
2026-04-29 08:19:38 -0400

    added function to create matrix of all genres

---
+++ src/features/content.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def build_genre_matrix(movies: pd.DataFrame):
+    all_genres = sorted({g for genres in movies["genres"] for g in genres})
+
+    genre_to_index = {genre: i for i, genre in enumerate(all_genres)}
+
+    genre_matrix = []
+
+    for genres in movies["genres"]:
+        vector = [0] * len(all_genres)
+
+        for g in genres:
+            vector[genre_to_index[g]] = 1
+        genre_matrix.append(vector)
+
+    genre_df = pd.DataFrame(genre_matrix, columns=all_genres)
+    genre_df["movieId"] = movies["movieId"].values
+
+    return genre_df
